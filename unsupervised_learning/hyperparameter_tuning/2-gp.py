beaa8aa1256a8c38a33565b4e2b58eb9e6af15cb
Carine-Ashimwe <carineashimwe@gmail.com>
2025-10-01 13:13:54 +0200

    ci:2. Update Gaussian Process

--- unsupervised_learning/hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
-import numpy as np
-from 1_gp import GaussianProcess  # if 1-gp.py in same folder
+"""
+Gaussian Process Update module
+"""
 
-class GaussianProcess(GaussianProcess):
-    """Extends GaussianProcess with update"""
+import numpy as np
+GP0 = __import__('1-gp').GaussianProcess
+
+
+class GaussianProcess(GP0):
+    """
+    Extends GaussianProcess with the ability to update
+    the process with new sample points.
+    """
 
     def update(self, X_new, Y_new):
-        """Updates GP with new sample"""
-        self.X = np.vstack((self.X, X_new.reshape(-1, 1)))
-        self.Y = np.vstack((self.Y, Y_new.reshape(-1, 1)))
+        """
+        Updates the Gaussian process with new sample points.
+
+        Parameters
+        ----------
+        X_new : np.ndarray of shape (1,)
+            New input sample.
+        Y_new : np.ndarray of shape (1,)
+            Output of the black-box function for X_new.
+        """
+        # Ensure column vector
+        X_new = X_new.reshape(-1, 1)
+        Y_new = Y_new.reshape(-1, 1)
+
+        # Update X and Y
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+
+        # Update covariance matrix K
         self.K = self.kernel(self.X, self.X)
 
